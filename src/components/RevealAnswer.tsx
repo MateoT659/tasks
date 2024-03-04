9febb6fc932607291782e543c00c3f35dcdab969
Mateo Torres <mateot659@gmail.com>
2024-03-04 16:05:41 -0500

    finished rest of components

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,12 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [showing, setShowing] = useState<boolean>(false);
+
+    return (
+        <div>
+            <Button onClick={() => setShowing(!showing)}>Reveal Answer</Button>{" "}
+            {showing && <p>42</p>}
+        </div>
+    );
 }
