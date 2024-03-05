ec81509c4ccceb1dff9cb5461918790cc054c755
Mateo Torres <mateot659@gmail.com>
2024-03-05 10:43:16 -0500

    margin

--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -24,7 +24,7 @@ function MoveableBox({ position }: { position: number }): JSX.Element {
                 border: "1px solid blue",
                 display: "inline-block",
                 verticalAlign: "bottom",
-                marginLeft: "px" + position
+                marginLeft: position + "px"
             }}
         ></div>
     );
